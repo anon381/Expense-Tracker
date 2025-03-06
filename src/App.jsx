b6669cc17244c0a9212c42245bfc14c7254d9d34
Yabets Maregn <yabetsanon@gmail.com>
2025-03-06 09:25:30 +0000

    docs: add TODO placeholder in App

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ export default function App() {
     <div style={{ padding: '2rem', fontFamily: 'system-ui' }}>
       <h1>React App Starter</h1>
       <p>Replace this placeholder with your application UI.</p>
+      {/* TODO: Add login form & transactions list wiring */}
     </div>
   );
 }
