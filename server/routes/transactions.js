c000eb7059e66d9bffa24cfae1041d15dc2c8a8e
Yabets Maregn <yabetsanon@gmail.com>
2025-05-11 09:15:00 +0900

    chore: remove transactions routes header

--- server/routes/transactions.js
+++ server/routes/transactions.js
@@ -1,4 +1,3 @@
-// Transactions API routes: protected by JWT; supports CRUD, filtering & summary.
 import { Router } from 'express';
 import { requireAuth } from '../middleware/auth.js';
 import { initTransactionStore, listTransactions, createTransaction, updateTransaction, deleteTransaction } from '../store/transactionStore.js';
